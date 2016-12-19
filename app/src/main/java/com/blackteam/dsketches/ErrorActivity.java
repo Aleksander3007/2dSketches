f1ad3b0081df46b416627f38b242fcc490e570e9
Aleksander Ermakov <AlexErmakov3007@ya.ru>
2016-12-19 14:53:53 +0400

    Added a missing ErrorActivity.

---
+++ app/src/main/java/com/blackteam/dsketches/ErrorActivity.java
@@ -0,0 +1,21 @@
+package com.blackteam.dsketches;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Для вывода исключений (ошибок приложения).
+ */
+public class ErrorActivity extends Activity {
+
+    public static final String ERROR_DATA = "error";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.error_layout);
+        String error_text = getIntent().getExtras().getString(ERROR_DATA);
+        ((TextView) findViewById(R.id.tv_exception_text)).setText(error_text);
+    }
+}
