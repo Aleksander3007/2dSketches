ef2815bf2b47a1f0e2bb07dc5752fd6e8fa855ff
Aleksander Ermakov <AlexErmakov3007@ya.ru>
2017-03-07 12:57:55 +0400

    Deleted unused classes.

--- app/src/main/java/com/blackteam/dsketches/gui/GameImage.java
+++
@@ -1,9 +0,0 @@
-package com.blackteam.dsketches.gui;
-
-import com.blackteam.dsketches.utils.Vector2;
-
-public class GameImage extends DisplayableObject {
-    public GameImage(Vector2 pos, Texture texture) {
-        super(pos, texture);
-    }
-}
